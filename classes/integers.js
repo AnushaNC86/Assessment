a977239e3a98b652a41a36e115533d6c352958e0
AnushaNC86 <anusha.nc@robosoftin.com>
2022-09-20 14:04:37 +0530

    Saved

--- classes/integers.js
+++ classes/integers.js
@@ -7,8 +7,8 @@ class Stack {
         return this.items[this.items.length - 1];
     }
 
-    add(element) {
-        return this.items.push(element);
+    add(ele) {
+        return this.items.push(ele);
     }
 
     remove() {
@@ -29,9 +29,9 @@ class Stack {
 let stack = new Stack();
 stack.add(1);
 stack.add(2);
+stack.add(3);
 stack.add(4);
-stack.add(7);
-console.log(stack.items);
+console.log("Stack items",stack.items);
 
 stack.remove();
 this.items = [5];
@@ -52,15 +52,6 @@ const removeOdd = (myArray) => {
     };
     return myArray;
 }
-resultArray = removeOdd(stack.items)
-console.log(resultArray);
+resultArr = removeOdd(stack.items)
+console.log(resultArr);
 
-const duplicateStack = (myStack) => {
-    let duplicate = new Stack();
-    for (let i = 0; i < stack.size(); i++) {
-        duplicate.add(myStack[i]);
-    }
-    return duplicate;
-}
-resultArray = duplicateStack(stack.items)
-console.log(resultArray);
